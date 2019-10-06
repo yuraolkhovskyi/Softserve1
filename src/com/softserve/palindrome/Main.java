f6e90313e25a8518f99d19a482ee3c372e06f57a
Yura Olkhovskyi <yuriy_olkhovskyi@i.ua>
2019-10-06 16:30:34 +0300

    added test and documentation

--- src/com/softserve/palindrome/Main.java
+++ src/com/softserve/palindrome/Main.java
@@ -1,7 +1,7 @@
 package com.softserve.palindrome;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Palindrome palindrome = new Palindrome();
         palindrome.start();
     }
--- src/com/softserve/palindrome/Palindrome.java
+++ src/com/softserve/palindrome/Palindrome.java
@@ -2,24 +2,49 @@ package com.softserve.palindrome;
 
 import java.util.Scanner;
 
+/**
+ * <p>The next class provides functionality for finding the
+ * longest palindromic sequence in a string which was
+ * entered by a user
+ * @author Yuriy Olkhovskyi
+ */
 public class Palindrome {
 
-    public void start() {
-        //some comments
-        //hello
-        //added new branch olkh
+    /** point from which the program starts
+     */
+     public void start() {
         String enteredString = getString();
-        printResults(longestPalindrome(enteredString));
+        printResults(findLengthOfLongestPalindrome(enteredString));
     }
 
-    private String getString() {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Input your string: ");
-        String str = in.nextLine();
-        return str;
+    /**
+     * The method read a string from a user
+     * @return String which was input by a user
+     */
+    public String getString() {
+        String str = "";
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Input: ");
+            str = in.nextLine();
+            if (str.length() > 100) {
+                System.out.println("You've entered more than 100 symbols, try again:");
+            } else {
+                return str;
+            }
+        }
     }
 
-    private int longestPalindrome(String enteredString) {
+    /**
+     *
+     * @param enteredString a string which is entered by a user
+     * @return int the length of the longest palindomic sequence
+     */
+    public int findLengthOfLongestPalindrome(String enteredString) {
+        if (enteredString.isEmpty()) {
+            return 0;
+        }
+        enteredString = enteredString.toLowerCase();
         String longestPalindrome = enteredString.substring(0, 1);
         for (int i = 0; i < enteredString.length(); i++) {
             String newPalindrome = checkIfEqual(enteredString, i, i);
@@ -34,7 +59,16 @@ public class Palindrome {
         return longestPalindrome.length();
     }
 
-    private static String checkIfEqual(String enteredString, int start, int end) {
+    /**
+     *
+     * @param enteredString a string which is entered by a user
+     * @param start a start symbol from which comparing is started, it goes left
+     *              comparing a correspond element from the right
+     * @param end an end symbol from which comparing is started, it goes right
+     *      *              comparing a correspond element from the left
+     * @return substring which is the longest palindrome from the symbol its started comparing
+     */
+    public String checkIfEqual(String enteredString, int start, int end) {
         while ((start >= 0 && end < enteredString.length()) && (enteredString.charAt(start) == enteredString.charAt(end))) {
             start--;
             end++;
@@ -42,8 +76,12 @@ public class Palindrome {
         return enteredString.substring(start + 1, end);
     }
 
-    private void printResults(int numberOfSymbols){
-        System.out.println("The longest palindrome has " + numberOfSymbols+" symbols");
+    /**
+     * Prints the result of the longest palindromic sequence
+     * @param numberOfSymbols of the longest sequence
+     *
+     */
+    private void printResults(int numberOfSymbols) {
+        System.out.println("Output:" + numberOfSymbols);
     }
 }
-
